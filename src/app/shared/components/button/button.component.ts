82692d75e19d6e884e4068220d5e299944161683
twic <illyadeveloper@gmail.com>
2020-05-14 00:52:57 +0300

    Stage: Sign in

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+
+  @Input() typeButton = 'submit';
+  @Input() disabled = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StatsProgressBarComponent } from './components/stats-progress-bar/stats-progress-bar.component';
-import { ChartCardComponent } from './components/chart-card/chart-card.component';
-import { InfoCardComponent } from './components/info-card/info-card.component';
-import {FlexModule} from '@angular/flex-layout';
-import { CameraPreviewComponent } from './components/camera-preview/camera-preview.component';
-import { StatsListComponent } from './components/stats-list/stats-list.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {StatsProgressBarComponent} from './components/stats-progress-bar/stats-progress-bar.component';
+import {ChartCardComponent} from './components/chart-card/chart-card.component';
+import {InfoCardComponent} from './components/info-card/info-card.component';
+import {FlexModule, GridModule} from '@angular/flex-layout';
+import {CameraPreviewComponent} from './components/camera-preview/camera-preview.component';
+import {StatsListComponent} from './components/stats-list/stats-list.component';
+import {ButtonComponent} from './components/button/button.component';
+import {InputComponent} from './components/input/input.component';
 
 
 @NgModule({
-  declarations: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent],
+  declarations: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, ButtonComponent, InputComponent],
   imports: [
     CommonModule,
-    FlexModule
+    FlexModule,
+    GridModule
   ],
-  exports: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent]
+  exports: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, InputComponent, ButtonComponent]
 })
-export class SharedModule { }
+export class SharedModule {
+}

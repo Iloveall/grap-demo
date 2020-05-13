9661f395f1a3263e23f6b03023f7c805f905ae35
Aleksandr Semenov <lovealldevelop@gmail.com>
2020-05-14 00:16:09 +0300

    Adds chart logic

--- src/app/charts/components/chart-line/chart-line.component.ts
+++ src/app/charts/components/chart-line/chart-line.component.ts
@@ -5,9 +5,6 @@ import {scaleLinear, scaleTime} from 'd3-scale';
 import {axisLeft, axisBottom} from 'd3-axis';
 import {min, max, bisector} from 'd3-array';
 import {line} from 'd3-shape';
-import {zoom, zoomIdentity} from 'd3-zoom';
-
-import * as d3 from 'd3';
 
 @Component({
   selector: 'app-chart-line',
@@ -17,18 +14,71 @@ import * as d3 from 'd3';
 export class ChartLineComponent implements OnInit, AfterViewInit {
   @ViewChild('chart', {static: true}) public chartRef: ElementRef;
 
+  chartData = [{
+    name: 'Some name',
+    color: '#00fff6',
+    values: [
+      {
+        timeStamp: 'Wed May 13 2020 09:00:00 GMT+0300 (GMT+03:00)',
+        value: 1000
+      },
+      {
+        timeStamp: 'Wed May 13 2020 15:00:00 GMT+0300 (GMT+03:00)',
+        value: 1000
+      },
+      {
+        timeStamp: 'Wed May 13 2020 15:01:00 GMT+0300 (GMT+03:00)',
+        value: 6000
+      },
+      {
+        timeStamp: 'Wed May 13 2020 16:00:00 GMT+0300 (GMT+03:00)',
+        value: 3000
+      },
+      {
+        timeStamp: 'Wed May 13 2020 23:00:00 GMT+0300 (GMT+03:00)',
+        value: 1000
+      }
+    ]
+  }, {
+    name: 'Some name',
+    color: '#ff0000',
+    values: [
+      {
+        timeStamp: 'Wed May 13 2020 09:00:00 GMT+0300 (GMT+03:00)',
+        value: 1200
+      },
+      {
+        timeStamp: 'Wed May 13 2020 12:00:00 GMT+0300 (GMT+03:00)',
+        value: 1400
+      },
+      {
+        timeStamp: 'Wed May 13 2020 14:01:00 GMT+0300 (GMT+03:00)',
+        value: 1500
+      },
+      {
+        timeStamp: 'Wed May 13 2020 17:00:00 GMT+0300 (GMT+03:00)',
+        value: 1200
+      },
+      {
+        timeStamp: 'Wed May 13 2020 23:00:00 GMT+0300 (GMT+03:00)',
+        value: 1000
+      }
+    ]
+  }];
+
   @Input() set data(data) {
     if (!data) {
       return;
     }
 
-    // this.draw();
+    this.chartData = data;
   }
 
-  maxYScaleValue = 70000;
+  minYScaleValue = 1000;
+  maxYScaleValue = 7000;
 
-  width: 300;
-  height: 300;
+  width: number;
+  height: number;
 
   xScale: any;
   yScale: any;
@@ -36,233 +86,165 @@ export class ChartLineComponent implements OnInit, AfterViewInit {
   xAxis: any;
   yAxis: any;
 
-  constructor() {
+  $xAxis: any;
+  $yAxis: any;
+
+  svg: any;
+  container: any;
+  options = {width: 0, height: 200};
+  margin = {top: 10, right: 10, bottom: 30, left: 40};
+
+  path: any;
+  linesG: any;
+  mouseG: any;
+
+  constructor(private el: ElementRef) {
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    // this.draw();
+    setTimeout(() => {
+      this.options.width = this.el.nativeElement.clientWidth;
+      this.createChart();
+    }, 250);
   }
 
-/*
-  draw(): void {
-// set the dimensions and margins of the graph
-    const margin = {top: 10, right: 100, bottom: 30, left: 30};
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+  createChart(): void {
+    this.width = this.options.width - this.margin.left - this.margin.right;
+    this.height = this.options.height - this.margin.top - this.margin.bottom;
 
-// append the svg object to the body of the page
-    const svg = d3.select(this.chartRef.nativeElement)
+    this.svg = select(this.chartRef.nativeElement)
       .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
+      .attr('width', this.options.width)
+      .attr('height', this.options.height);
+
+    this.container = this.svg
       .append('g')
-      .attr('transform',
-        'translate(' + margin.left + ',' + margin.top + ')');
+      .attr('class', 'container')
+      .attr('transform', 'translate(' + this.margin.left + ', ' + this.margin.top + ')');
 
-// Read the data
-    // @ts-ignore
-    d3.csv('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_connectedscatter.csv', (data: any) => {
+    this.createX();
+    this.createXAxis();
+    this.createY();
+    this.createYAxis();
+    this.createLinesGroup();
+    this.createMousePointer();
 
-      data = [data];
-
-      console.log('data', data);
-
-      // List of groups (here I have one group per column)
-      const allGroup = ['valueA', 'valueB', 'valueC'];
-
-      // Reformat the data: we need an array of arrays of {x, y} tuples
-      const dataReady = allGroup.map((grpName) => { // .map allows to do something for each element of the list
-        return {
-          name: grpName,
-          values: data.map((d) => {
-            return {time: d.time, value: +d[grpName]};
-          })
-        };
-      });
-      // I strongly advise to have a look to dataReady with
-      // console.log(dataReady)
-
-      // A color scale: one color for each group
-      const myColor = d3.scaleOrdinal()
-        .domain(allGroup)
-        .range(d3.schemeSet2);
-
-      // Add X axis --> it is a date format
-      const x = d3.scaleLinear()
-        .domain([0, 10])
-        .range([0, width]);
-      svg.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(x));
-
-      // Add Y axis
-      const y = d3.scaleLinear()
-        .domain([0, 20])
-        .range([height, 0]);
-      svg.append('g')
-        .call(d3.axisLeft(y));
-
-      // Add the lines
-      // tslint:disable-next-line:no-shadowed-variable
-      const line = d3.line()
-        .x((d: any) => {
-          return x(+d.time);
-        })
-        .y((d: any) => {
-          return y(+d.value);
-        });
-      // @ts-ignore
-      svg.selectAll('myLines')
-        .data(dataReady)
-        .enter()
-        .append('path')
-        .attr('class', (d) => {
-          return d.name;
-        })
-        .attr('d', (d: any) => {
-          return line(d.values);
-        })
-        // @ts-ignore
-        .attr('stroke', (d: any) => {
-          return myColor(d.name);
-        })
-        .style('stroke-width', 4)
-        .style('fill', 'none');
-
-      // Add the points
-      // @ts-ignore
-      svg
-        // First we need to enter in a group
-        .selectAll('myDots')
-        .data(dataReady)
-        .enter()
-        .append('g')
-        // @ts-ignore
-        .style('fill', (d: any) => {
-          return myColor(d.name);
-        })
-        .attr('class', (d: any) => {
-          return d.name;
-        })
-        // Second we need to enter in the 'values' part of this group
-        .selectAll('myPoints')
-        .data((d) => {
-          return d.values;
-        })
-        .enter()
-        .append('circle')
-        .attr('cx', (d) => {
-          return x(d.time);
-        })
-        .attr('cy', (d) => {
-          return y(d.value);
-        })
-        .attr('r', 5)
-        .attr('stroke', 'white');
-
-      // Add a label at the end of each line
-      svg
-        .selectAll('myLabels')
-        .data(dataReady)
-        .enter()
-        .append('g')
-        .append('text')
-        .attr('class', (d) => {
-          return d.name;
-        })
-        .datum((d) => {
-          return {name: d.name, value: d.values[d.values.length - 1]};
-        }) // keep only the last value of each time series
-        .attr('transform', (d) => {
-          return 'translate(' + x(d.value.time) + ',' + y(d.value.value) + ')';
-        }) // Put the text at the position of the last point
-        .attr('x', 12) // shift the text a bit more right
-        .text((d) => {
-          return d.name;
-        })
-        // @ts-ignore
-        .style('fill', (d: any) => {
-          return myColor(d.name);
-        })
-        .style('font-size', 15);
-
-      // Add a legend (interactive)
-      // @ts-ignore
-      // @ts-ignore
-      svg
-        .selectAll('myLegend')
-        .data(dataReady)
-        .enter()
-        .append('g')
-        .append('text')
-        .attr('x', (d, i) => {
-          return 30 + i * 60;
-        })
-        .attr('y', 30)
-        .text((d) => {
-          return d.name;
-        })
-        // @ts-ignore
-        .style('fill', (d: any) => {
-          return myColor(d.name);
-        })
-        .style('font-size', 15)
-        .on('click', (d) => {
-          // is the element currently visible ?
-          const currentOpacity = d3.selectAll('.' + d.name).style('opacity');
-          // Change the opacity: from 0 to 1 or from 1 to 0
-          // @ts-ignore
-          d3.selectAll('.' + d.name).transition().style('opacity', currentOpacity === 1 ? 0 : 1);
-
-        });
-    });
-
+    this.drawLinesData();
   }
-*/
 
-  /*  createX(): void {
-      this.xScale = scaleTime()
-        .domain([
-          new Date('Wed May 13 2020 09:00:00 GMT+0300 (GMT+03:00)'),
-          new Date('Wed May 13 2020 23:00:00 GMT+0300 (GMT+03:00)')
-        ])
-        .range([0, this.width]);
-    }
+  createX(): void {
+    this.xScale = scaleTime()
+      .domain([
+        new Date('Wed May 13 2020 09:00:00 GMT+0300 (GMT+03:00)'),
+        new Date('Wed May 13 2020 23:00:00 GMT+0300 (GMT+03:00)')
+      ])
+      .range([0, this.width]);
+  }
 
-    createY(): void {
-      this.yScale = scaleLinear()
-        .domain([0, this.maxYScaleValue])
-        .range([this.height, 0]);
-    }
+  createXAxis(): void {
+    this.xAxis = axisBottom(this.xScale)
+      .ticks(5)
+      .tickSize(-this.height);
 
-    createXAxis(): void {
-      this.xAxis = axisBottom(this.xScale)
-        .ticks(5)
-        .tickFormat((date: any) => {
-          return this.d3Service.getTimeFormat(date);
-        });
+    this.$xAxis = this.container
+      .append('g')
+      .attr('class', 'gp-axis__x')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(this.xAxis);
+  }
 
-      this.$xAxis = this.container
-        .append('g')
-        .attr('class', 'gp-axis__x')
-        .attr('transform', 'translate(0,' + this.height + ')')
-        .call(this.xAxis);
-    }
+  createY(): void {
+    this.yScale = scaleLinear()
+      .domain([this.minYScaleValue, this.maxYScaleValue])
+      .range([this.height, 0]);
+  }
 
-    createYAxis(): void {
-      this.yAxis = axisLeft(this.yScale)
-        .tickValues(this.d3Service.generateTickValues(0, this.maxYScaleValue, this.yScaleStep, this.isNegativeAxis))
-        .tickSize(-this.width)
-        .tickFormat((value) => `${ value }`);
+  createYAxis(): void {
+    this.yAxis = axisLeft(this.yScale)
+      .tickSize(1000)
+      .tickSize(-this.width)
+      .tickFormat((value) => `${value}`);
 
-      this.$yAxis = this.container
-        .append('g')
-        .attr('class', 'gp-axis__y')
-        .attr('width', this.width)
-        .call(this.yAxis);
-    }*/
+    this.$yAxis = this.container
+      .append('g')
+      .attr('class', 'gp-axis__y')
+      .attr('width', this.width)
+      .call(this.yAxis);
+  }
+
+  createLinesGroup(): void {
+    this.linesG = this.container.append('g');
+  }
+
+  drawLinesData() {
+    this.path = this.linesG
+      .selectAll('.path-data')
+      .data(this.chartData);
+
+    this.path
+      .enter()
+      .append('path')
+      .attr('class', 'path-data')
+      .merge(this.path)
+      .attr('d', (group: any) => {
+        return line()
+          .x((d: any) => {
+            return this.xScale(new Date(d.timeStamp));
+          })
+          .y((d: any) => {
+            return this.yScale(d.value);
+          })(group.values);
+      })
+      .attr('fill', 'none')
+      .attr('stroke', (group: any) => {
+        return group.color;
+      })
+      .attr('stroke-width', 3)
+      .style('clip-path', 'url(#clip)');
+
+    this.path
+      .exit()
+      .remove();
+  }
+
+  createMousePointer(): void {
+    this.mouseG = this.container
+      .append('g')
+      .attr('class', 'mouse-over-effects');
+
+    this.mouseG.append('path')
+      .attr('class', 'mouse-line')
+      .attr('stroke', '#A0A0A0')
+      .attr('stroke-width', '1px');
+
+    this.mouseG.append('rect')
+      .attr('class', 'mouse-rect')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .attr('fill', 'none')
+      .attr('pointer-events', 'all');
+
+    this.mouseG
+      .on('mouseover', () =>
+        this.mouseG.select('.mouse-line').style('display', null)
+      )
+      .on('mouseout', () =>
+        this.mouseG.select('.mouse-line').style('display', 'none')
+      )
+      .on('mousemove', (_, j, nodes) => {
+        const position =  mouse(nodes[0])[0];
+
+        this.svg
+          .selectAll('.mouse-line')
+          .attr('d', () => {
+            let d = 'M' + position + ',' + this.height;
+            d += ' ' + position + ',' + 0;
+            return d;
+          });
+      });
+  }
 
 }

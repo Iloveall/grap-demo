e50bb11d3c573e0a1ffe8816b193d05ef306e56d
twic <illyadeveloper@gmail.com>
2020-05-14 03:10:51 +0300

    Stage: header

--- src/app/main-layout/main-layout.module.ts
+++ src/app/main-layout/main-layout.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarLeftComponent } from './components/sidebar-left/sidebar-left.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MainLayoutComponent} from './components/main-layout/main-layout.component';
+import {HeaderComponent} from './components/header/header.component';
+import {SidebarLeftComponent} from './components/sidebar-left/sidebar-left.component';
 import {RouterModule} from '@angular/router';
+import {SharedModule} from '../shared/shared.module';
+import {FlexModule, GridModule} from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [MainLayoutComponent, HeaderComponent, SidebarLeftComponent],
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule,
+    RouterModule,
+    FlexModule,
+    GridModule
   ]
 })
-export class MainLayoutModule { }
+export class MainLayoutModule {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,17 +9,18 @@ import { StatsListComponent } from './components/stats-list/stats-list.component
 import {NgbProgressbarModule} from '@ng-bootstrap/ng-bootstrap';
 import {ButtonComponent} from './components/button/button.component';
 import {InputComponent} from './components/input/input.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
-  declarations: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, ButtonComponent, InputComponent],
+  declarations: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, ButtonComponent, InputComponent, SearchComponent],
   imports: [
     CommonModule,
     NgbProgressbarModule,
     FlexModule,
     GridModule
   ],
-  exports: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, InputComponent, ButtonComponent]
+  exports: [StatsProgressBarComponent, ChartCardComponent, InfoCardComponent, CameraPreviewComponent, StatsListComponent, InputComponent, ButtonComponent, SearchComponent]
 })
 export class SharedModule {
 }
